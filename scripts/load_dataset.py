1f3d4e20d13040b0da106ded3e25160988cbd654
mussewold <mussenigussie@gmail.com>
2024-12-24 08:33:43 +0300

    intreme log

---
+++ scripts/load_dataset.py
@@ -0,0 +1,91 @@
+# scripts/load_data.py
+
+import os
+import psycopg2
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+
+# Load environment variables from .env file
+load_dotenv('../.env')
+
+# Fetch database connection parameters from environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+def load_data_from_postgres(query):
+    """
+    Connects to the PostgreSQL database and loads data based on the provided SQL query.
+
+    :param query: SQL query to execute.
+    :return: DataFrame containing the results of the query.
+    """
+    try:
+        # Establish a connection to the database
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+
+        # Load data using pandas
+        df = pd.read_sql_query(query, connection)
+
+        # Close the database connection
+        connection.close()
+
+        return df
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+
+def load_data_using_sqlalchemy(query):
+    """
+    Connects to the PostgreSQL database and loads data based on the provided SQL query using SQLAlchemy.
+
+    :param query: SQL query to execute.
+    :return: DataFrame containing the results of the query.
+    """
+    try:
+        # Create a connection string
+        connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+        # Create an SQLAlchemy engine
+        engine = create_engine(connection_string)
+
+        # Load data into a pandas DataFrame
+        df = pd.read_sql_query(query, engine)
+
+        return df
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+
+def load_data():
+
+    # Define your SQL query
+    query = "SELECT * FROM xdr_data;"  # Replace with your actual table name
+
+    # Load data from PostgreSQL using SQLAlchemy
+    df = load_data_using_sqlalchemy(query)
+
+    # Display the first few rows of the dataframe
+    if df is not None:
+        print("Successfully loaded the data")
+    else:
+        print("Failed to load data.")
+
+    # Set the option to display all columns
+    pd.set_option('display.max_columns', None)
+    return df 
